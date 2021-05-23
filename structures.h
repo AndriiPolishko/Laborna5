787c5afc72bf0f719dcbc113cb7eb0c4606eff39
Zepee <ramp8a799@gmail.com>
2021-05-23 22:10:18 +0300

    Added structure for initial spots

---
+++ structures.h
@@ -0,0 +1,15 @@
+using  namespace std;
+struct Spot {
+    float latitude;
+    float longitude;
+    string type;
+    string subtype;
+    string name;
+    string address;
+    double x,y;
+    void adapt() {
+        //converting into xOy coordinates
+        x = 6371 * cos(latitude * 3.14159 / 180.) * (longitude * 3.14159 / 180.);
+        y = (latitude * 3.14159 / 180.) * 6371;
+    }
+};
