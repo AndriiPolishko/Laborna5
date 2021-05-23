e6aecb620c39cec408349dfb08c3c868a74f3ce8
Zepee <ramp8a799@gmail.com>
2021-05-23 22:12:34 +0300

    Error solved

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-using  namespace std;
+
+#include "structures.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
--- structures.h
+++ structures.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include "math.h"
+using  namespace std;
 using  namespace std;
 struct Spot {
     float latitude;
