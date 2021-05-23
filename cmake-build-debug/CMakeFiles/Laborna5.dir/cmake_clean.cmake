058c76d51e528a77f52a43b59b233409329027e5
Zepee <ramp8a799@gmail.com>
2021-05-23 22:02:39 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Laborna5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laborna5.dir/main.cpp.obj"
+  "Laborna5.exe"
+  "Laborna5.exe.manifest"
+  "Laborna5.pdb"
+  "libLaborna5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laborna5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Laborna5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Laborna5.
+# This may be replaced when dependencies are built.
